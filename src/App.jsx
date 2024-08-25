fde35fd98f65e3ef344811541814ff39a8a4f86d
Amiran Ivanidze <amiran.ivanidz3@gmail.com>
2024-08-25 17:28:43 +0400

    created components and base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline boxShadow">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero
+
+    </section>
+
+    <section className="padding">
+      PupularProduct
+    </section>
+    
+    <section className="padding">
+      SuperQualty
+    </section>
+    
+    <section className="padding-x py-10">
+      Services
+    </section>
+    
+    <section className="padding">
+      SpecialOffers
+    </section>
+
+    <section className="bg-pale-blue padding">customer eviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">subscribe 1</section>
+    <section className="bg-black padding-x padding-t pb-8"></section>
+
+  </main>
+)
+
+
+export default App;
